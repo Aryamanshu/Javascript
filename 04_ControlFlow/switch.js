a59f92005ae050a7c7ec61810ac1081dcf3c7979
Aryamanshu Mishra <93377648+Aryamanshu@users.noreply.github.com>
2024-06-28 09:39:09 +0530

    Add files via upload

---
+++ 04_ControlFlow/switch.js
@@ -0,0 +1,32 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// basic syntax of switch
+
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("january")
+        break;
+    case 2:
+        console.log("feb")
+        break;
+    case 3:
+        console.log("march")
+        break;
+    case 4:
+        console.log("april")
+        break;
+
+    default:
+        console.log("month not found");
+        break;
+}
