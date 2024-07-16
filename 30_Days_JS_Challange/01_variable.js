3a445552bdcbb188f2024accdd10383d13c18e6a
Aryamanshu Mishra <93377648+Aryamanshu@users.noreply.github.com>
2024-07-16 08:01:27 +0530

    Create 01_variable.js

---
+++ 30_Days_JS_Challange/01_variable.js
@@ -0,0 +1,58 @@
+//actv-1 
+//Task-1
+var num = 12
+console.log(num);
+
+//Task-2
+let Str1 = "aryanmishra"
+console.log(Str1);
+
+//actv-2
+//Task-1
+
+const chai = true
+console.log(chai);
+
+//actv-3
+
+const numb = 12345
+const str2 = "heybhai"
+const bool = false
+const obj = {
+    email: "aryanbvhai@gmail.com",
+    pass: "12345"
+}
+const arr = [1,2,3,4,5]
+
+console.log(typeof num);
+console.log(typeof str2);
+console.log(typeof bool);
+console.log(typeof obj);
+console.log(typeof arr);  // is also object
+
+//actv-4
+
+let var2 = 23;
+console.log("Initial value: ", var2);
+
+var2 = 30;
+
+console.log("Reassigned value: ", var2);
+
+//actv-5
+
+// const var3 = 23;
+// console.log("Initial value: ", var3);
+
+// var3 = 30; // we can't reassign the value when we r using const
+
+// console.log("Reassigned value: ", var3);
+
+
+//actv-6
+
+
+
+
+
+
