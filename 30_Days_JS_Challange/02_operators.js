23a51b73df60bd93f1bebbba9d3c501049205ae1
Aryamanshu Mishra <93377648+Aryamanshu@users.noreply.github.com>
2024-07-18 23:52:16 +0530

    Add files via upload

---
+++ 30_Days_JS_Challange/02_operators.js
@@ -0,0 +1,92 @@
+var num1 = 12
+var num2 = 13
+
+console.log(num1 + num2);
+
+var num3 = 12
+var num4 = 13
+
+console.log(num1 * num2);
+
+var num5 = 12
+var num6 = 13
+
+console.log(num6 - num5);
+
+var num7 = 5
+var num8 = 130
+
+console.log(num8 / num7);
+
+var hel1 = 88
+var hel2 = 10
+
+console.log(`remainder is ${hel1 % hel2}`);
+
+
+var add = 12
+add += 2
+
+console.log(add);
+
+var sub = 12
+sub -= 2
+
+console.log(sub);
+
+
+var comp = 12
+var comp2 = 67
+
+if(comp > comp2){
+console.log("true");
+
+}else {
+    console.log("false");
+}
+
+
+if(comp >= 10){
+ console.log("good");
+}else if(comp2 <= 50){
+console.log("nope");
+}else {
+    console.log("galat hai");
+}
+
+
+var men = 28
+var nem = 12
+
+if (men == nem){
+console.log("equla hai");
+}else {
+    console.log("equal nhi hai");
+}
+
+if(men === nem){
+    console.log("data type same hai");
+
+}else {
+    console.log("data type alag alag hai");
+}
+
+if(men > 30 && nem < 20){
+console.log("true");
+}else {
+console.log("false");
+}
+
+if(men > 30 || nem < 20){
+console.log("true");
+}else {
+console.log("false");
+}
+
+const a = 3;
+const b = -2;
+
+console.log(!(a > 0 || b > 0));
+
+//ternary operator (?)
+
