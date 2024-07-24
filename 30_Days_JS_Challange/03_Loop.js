11a9ae439d42e42149488e09f67f076350895c72
Aryamanshu Mishra <93377648+Aryamanshu@users.noreply.github.com>
2024-07-24 23:14:12 +0530

    Add files via upload

---
+++ 30_Days_JS_Challange/03_Loop.js
@@ -0,0 +1,107 @@
+// approach-1
+
+// const Num = 10
+// function loop() {
+//     for( var i = 0; i <= Num; i++){
+//         console.log(i);
+//     }
+      
+//     }
+// loop()
+
+
+// aproach-2
+
+// function loop() {
+//     for( var i = 1; i <= 10; i++){
+//         console.log(i);
+//     }
+      
+//     }
+// loop()
+
+// approach-3
+
+
+    // for( var i = 1; i <= 10; i++){
+    //     console.log(i);
+    // }
+
+
+
+    // multiplication of 5 
+
+// for(var i = 1; i<=10; i++){
+//     console.log(i*5);
+// }
+
+
+
+// sum of numbers 1-10
+
+// let sum = 0;
+// let i = 1;
+
+// do {
+//   sum += i;
+//   i++;
+// } while (i <= 10);
+
+// console.log("The sum of numbers from 1 to 10 is:", sum);
+
+
+// print number from 10 to 1 using do while loopp
+
+// let i =10;
+//  do {
+//     console.log(i);
+//     i--;
+//  } while (i >= 1);
+
+
+
+// factorial of number 
+
+// let n=6, i=1, fact=1
+// while(i<=n)
+// {
+//   fact = fact*i 
+//   i++
+// }
+// console.log(fact)
+
+
+// nested for loo pattern printing
+
+// let rows = 5; // Adjust this value to change the number of rows
+
+// for (let i = 1; i <= rows; i++) {
+//   let stars = "";
+//   for (let j = 1; j <= i; j++) {
+//     stars += "*";
+//   }
+//   console.log(stars);
+// }
+
+
+// skip the numbr 5
+
+// for(var i = 1; i<=10; i++){
+//     if(i == 5){
+//         continue;
+        
+//     }
+//     console.log(i);
+// }
+
+
+// stop the loop when number is 7
+
+
+for(var i = 1; i<=10; i++){
+    if(i == 7){
+        break;
+        
+    }
+    console.log(i);
+}
